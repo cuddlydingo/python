ea3d25a62faf03548e4a083f509bd6b996008dd4
James Phillips <jphillips@ea.com>
2025-06-19 12:26:44 -0500

    Adding completed exercises for Module 2

---
+++ Module_2/ex18_demo.py
@@ -0,0 +1,12 @@
+def do_something():
+    print("I did something!")
+
+
+def do_more_things(a, b):
+    print("A IS", a, "B IS", b)
+
+
+# now we can call it by its name
+do_something()
+
+do_more_things("hello", 1)
