67aedc26893c892eec4f9b26ba404d9f968d1dcc
James Phillips <jphillips@ea.com>
2025-06-23 13:10:36 -0500

    adding daily exercises for posterity

---
+++ Module_2/ex25.py
@@ -0,0 +1,8 @@
+def create_a_car(color, speed, transmission_type):
+    car_specs = {
+        "color": color, 
+        "speed": speed, 
+        "transmission_type": transmission_type
+        }
+
+    return car_specs
