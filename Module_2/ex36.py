523b9737e9e84e0b79f02c3d2d7752786ab39df0
James Phillips <jphillips@ea.com>
2025-07-20 20:56:41 -0400

    Adding completed exercises for Module 2

--- Module_2/ex36.py
+++ Module_2/ex36.py
@@ -1,3 +1,17 @@
 import sys
-import ex36_modules
+from ex36_modules import *
 
+print("Welcome to the Quest-iest Quest that ever Quested.\n\
+      It's dangerous out there, so first, some questions....")
+
+player_name = input("What... is your name? ")
+player_quest = input("What... is your quest? ")
+airspeed_velocity_of_coconut_laden_swallow = input("What... \
+    is the airspeed velocity of a coconut-laden swallow? ")
+
+if "african or european" in airspeed_velocity_of_coconut_laden_swallow:
+    explode_bridgekeeper()
+    exit(0)
+else:
+    print(f"Ahhhh, {player_name}... You may proceed.")
+    begin_journey()
--- Module_2/ex36_modules.py
+++ Module_2/ex36_modules.py
@@ -1,2 +1,42 @@
-def cave_of_wonders():
+def explode_bridgekeeper():
+    print("The BridgeKeeper explodes in a blaze of confusion.\n\
+        You may cross the bridge.")
+    begin_journey()
+
+def begin_journey():
+    print("You find yourself standing at the top of a great precipice.  \
+        Would you like to:\n\
+            A: Jump from the cliff screaming wildly\n\
+            B: Sing out and see if your voice echoes\n\
+            C: Use your binoculars to see what you can see")
     
+    choice = input("What do you choose? ")
+    
+    if "A" in choice:
+        die("You die screaming as your body sails through the clouds.")
+    if "B" in choice:
+        print("You hear nothing in return except the empty wind.")
+        exit(0)
+    if "C" in choice:
+        paraglider_answer = input("In the distance you see your goal.  Do you open your paraglider?")
+        if "yes" in paraglider_answer:
+            print("You sail spectacularly to the Cave of Wonders!")
+            cave_of_wonders()
+        else: 
+            "Ummm... nothing happens."
+            exit(0)
+
+
+def cave_of_wonders():
+    worthy_ans = input("You arrive at the cave of Wonders.  Are you worthy?")
+    if "diamond in the rough" in worthy_ans:
+        print("You win all the treasure!")
+        exit(0)
+    else:
+        print("You are eaten by a lion.  The end.")
+        exit(0)
+    
+
+def die(why):
+    print(why)
+    exit(0)
